b5600d3b0ce372c1baa22b40f0977f72ccb1bb10
Joegitrussell <112443220+Joegitrussell@users.noreply.github.com>
2022-11-03 15:16:53 +0000

    tests done

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
+export function fileTester(){
+        return "tested"
+}
+
 /* 
 👉 Write your kata here!
-*/
+"Take in a string (letters could be any combination of upper or lower case). Use the letters 
+numeric position in the standard alphabet (ie a=1, d=4) as a value.
+Write a function that multiplies each value by itself and then return the sum of all values"
+example: string = and. a=1, n=9, d=4. 1*1=1, 9*9=81, 4*4=16. 1+81+16=98
 
-//👉 Write the function your CodeWarriors will start with below here:
+//👉 Write the function your CodeWarriors will start with below here:*/
+
+export function stringSquareAndSum(string) {
+}
--- main.test.js
+++ main.test.js
@@ -1,1 +1,34 @@
 //👉 Write your tests below here:
+import { fileTester } from "./main.js";
+import { stringSquareAndSum } from "./main.js";
+import { test, expect } from "@jest/globals";
+
+test(`fileTester`, () => {
+    const actual = fileTester("test");
+    const expected = "tested";
+  expect(actual).toBe(expected);
+})
+
+test(`stringSquareAndSumTest1`, () => {
+    const actual = stringSquareAndSum("abcd");
+    const expected = 30;
+  expect(actual).toBe(expected);
+})
+
+test(`stringSquareAndSumTest2`, () => {
+    const actual = stringSquareAndSum("coding");
+    const expected = 576;
+  expect(actual).toBe(expected);
+})
+
+test(`stringSquareAndSumTest3`, () => {
+    const actual = stringSquareAndSum("WinTer");
+    const expected = 1555;
+  expect(actual).toBe(expected);
+})
+
+test(`stringSquareAndSumTest4`, () => {
+    const actual = stringSquareAndSum("SchoolOfCode");
+    const expected = 1564;
+  expect(actual).toBe(expected);
+})
